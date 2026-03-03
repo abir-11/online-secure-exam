3c2b462d4ef9f4d0c81faabf9ae4fb43348a90a5
Sadia1609 <sadia.rahman160920@gmail.com>
2026-03-03 23:49:02 +0600

    added PUT method for single user's api

--- src/app/api/admin/users/[id]/route.js
+++ src/app/api/admin/users/[id]/route.js
@@ -3,3 +3,64 @@ import { NextResponse } from "next/server";
 import { getCollection } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 import bcrypt from "bcryptjs";
+
+//GET
+export async function GET(request, { params }) {
+  try {
+    const { id } = params;
+
+    if (!ObjectId.isValid(id)) {
+      return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+    }
+
+    const usersCollection = await getCollection("users");
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const { password, ...userWithoutPassword } = user;
+    return NextResponse.json({ success: true, user: userWithoutPassword });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch user" },
+      { status: 500 },
+    );
+  }
+}
+
+//PUT
+export async function PUT(request, { params }) {
+  try {
+    const { id } = params;
+    const updates = await request.json();
+
+    if (!ObjectId.isValid(id)) {
+      return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+    }
+    const usersCollection = await getCollection("users");
+
+    const { _id, password, createdAt, ...validUpdates } = updates;
+    validUpdates.updatedAt = new Date();
+
+    if (updates.password) {
+      validUpdates.password = await bcrypt.hash(updates.password, 10);
+    }
+    const result = await usersCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: validUpdates },
+    );
+
+    if (result.matchedCount === 0) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ success: true, message: "User updated" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to update user" },
+      { status: 500 },
+    );
+  }
+}
