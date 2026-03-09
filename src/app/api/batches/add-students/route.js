18007fb65234d0036cfbd006f1c516b1c192e10e
Saima Khan <saimakhancsedu@gmail.com>
2026-03-09 15:29:51 +0600

    question shufffle done

--- src/app/api/batches/add-students/route.js
+++ src/app/api/batches/add-students/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getCollection } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 import { getServerSession } from "next-auth";
-// import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req) {
   try {
