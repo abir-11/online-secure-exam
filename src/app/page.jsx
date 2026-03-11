210329afc18df10f50f630884030abf1fb3fd10b
Abir <malam2331103@bscse.uiu.ac.bd>
2026-03-11 12:51:27 +0600

    some feature add home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,23 @@
+import AboutSection from "@/components/Home/AboutSection/page";
+import CourseCategories from "@/components/Home/CourseCategories/page";
 import Feature from "@/components/Home/Feature/Feature";
 import GetStarted from "@/components/Home/Get-Started/GetStarted";
 import HomeHero from "@/components/Home/Home-Hero/HomeHero";
+import Newsletter from "@/components/Home/Newsletter/page";
+import PricingSection from "@/components/Home/PricingSection/page";
 import Trusted from "@/components/Home/Trusted/Trusted";
 
 export default function Home() {
   return (
     <div>
       <HomeHero />
+      <AboutSection></AboutSection>
       <Trusted />
+      <CourseCategories></CourseCategories>
       <Feature />
+      <PricingSection></PricingSection>
       <GetStarted />
+      <Newsletter></Newsletter>
     </div>
   );
 }
